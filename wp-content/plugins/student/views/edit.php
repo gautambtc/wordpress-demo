df13037005bb20e752eca6755075ea27595bbae5
NenciMakawana-BTC <nenci.makwana@botreetechnologies.com>
2016-10-14 14:15:40 +0530

    - Add and Edit Student.

---
+++ wp-content/plugins/student/views/edit.php
@@ -0,0 +1,19 @@
+<form name="studentdetails" id='student' method="post" action = "" >
+  <p>
+    <label for="key"> FirstName  - </label>
+    <input type='text' id='firstname' name='firstname'/>
+  </p>
+  <p>
+    <label for="key"> LastName :</label>
+    <input type='text' id='lastname' name='lastname'/>
+  </p>
+  <p>
+    <label for="key"> Gender :</label>
+    <input type='text' id='gender' name='gender'/>
+  </p>
+  <p>
+    <label for="key"> DOB :</label>
+    <input type='text' id='dob' name='dob'/>
+  </p>
+  <input id="submit" type="submit" name="Submit" class="button button-primary" value="Submit" />
+</form>
