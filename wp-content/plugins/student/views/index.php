b04d5d09e366785c6671223189e9c4f65e4be58c
Gautam Moradiya <gautam.moradiya@botreetechnologies.com>
2016-10-20 11:16:19 +0530

    -add plugins

--- wp-content/plugins/student/views/index.php
+++ wp-content/plugins/student/views/index.php
@@ -1,7 +1,22 @@
+
+
+
 <?php
     global $query;
     $query="SELECT * FROM wp_student_details ";
     $students= $wpdb->get_results($query);
+    require_once(ABSPATH.'vendor/facebook/graph-sdk/src/Facebook/autoload.php');
+    $fb = new \Facebook\Facebook([
+      'app_id' => '1142047632538310',
+      'app_secret' => '84890d02cf6dd9f10b0ff70884039651',
+      'default_graph_version' => 'v2.8',
+      'default_access_token' => 'EAAQOr690OsYBAP8FXAg8wWNgljUeEsYzmodonWKO2lcS5IWdgZBshr3vH6bE9SBUJSeiVLaY6MtNblYc10ntan6RtU6VMhgMpE5YNreiYp4YyZAoAsN8E17SFOUdHk8evQQE9zpxAL9udnvPo39cgwv1zV28bNN88AZCVeZCHQZDZD', // optional
+    ]);
+    
+    $response = $fb->get('/messages');
+    print_r($response);
+    // print_r($response);
+    // var_dump($response);
 ?>
 
 <a href=<?= admin_url()."?page=student/views/new.php" ?>>New Sudent</a>
